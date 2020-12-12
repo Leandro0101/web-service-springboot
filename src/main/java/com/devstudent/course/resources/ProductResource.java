72922e6eff9c38f3992eac4a269515487117b7fb
Leandro0101 <leandrovieira3306@gmail.com>
2020-12-12 14:59:57 -0300

    product

---
+++ src/main/java/com/devstudent/course/resources/ProductResource.java
@@ -0,0 +1,36 @@
+package com.devstudent.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devstudent.course.entities.Product;
+import com.devstudent.course.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = service.findAll();
+		
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product obj = service.findById(id);
+		
+		return ResponseEntity.ok(obj);
+	}
+	
+}
---
+++ src/main/java/com/devstudent/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.devstudent.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devstudent.course.entities.Product;
+import com.devstudent.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> product = repository.findById(id);
+		
+		return product.get();
+	}
+}
