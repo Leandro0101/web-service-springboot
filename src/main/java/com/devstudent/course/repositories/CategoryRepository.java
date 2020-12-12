f2233302514fdc3cfa0af12e30c33c67e5d55489
Leandro0101 <leandrovieira3306@gmail.com>
2020-12-12 12:19:57 -0300

    Category

---
+++ src/main/java/com/devstudent/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.devstudent.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devstudent.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
