4aacb411828444cdd387325070dfc099d46b3d92
Leandro0101 <leandrovieira3306@gmail.com>
2020-12-26 17:32:28 -0300

    user delete

--- src/main/java/com/devstudent/course/resources/ProductResource.java
+++ src/main/java/com/devstudent/course/resources/ProductResource.java
@@ -1,5 +1,6 @@
 package com.devstudent.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devstudent.course.entities.Product;
 import com.devstudent.course.services.ProductService;
@@ -15,22 +17,24 @@ import com.devstudent.course.services.ProductService;
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
-	
+
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Product>> findAll(){
+	public ResponseEntity<List<Product>> findAll() {
 		List<Product> list = service.findAll();
-		
+
 		return ResponseEntity.ok(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Product> findById(@PathVariable Long id){
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
 		Product obj = service.findById(id);
-		
-		return ResponseEntity.ok(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(obj);
 	}
-	
+
 }
